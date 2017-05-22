20111a66e6bb3dad453be68c107dd07e8748948a
Christian <christian.etpr10@gmail.com>
2017-05-21 22:02:11 -0700

    Fetching messags from backend

--- client/app/components/CoquiList.js
+++ client/app/components/CoquiList.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class CoquiList extends React.Component {
         constructor(props) {
                 super(props);
                 this.state = {
                         coquis: [{
-                                message: 'This is an example coqui'
+                                message: 'Changing the a example coqui'
                         }]
                 }
+        }
+        componentDidMount() {
+                console.log('testing');
+                this.init()
+        }
+        init() {
+                axios.get(`/coqui`).then((res) => {
+                        this.setState({
+                                coquis: res.data.coquis
+                        })
+                })
         }
         render() {
                 return (
                         <ul className='list-group'>
                                 {this.state.coquis.map((coqui, index) => (
-                                        <li className="list-group-item" key={index}> {coqui.message} </li>
+                                        <li className="list-group-item" key={coqui.id}> {coqui.message} </li>
                                 ))}
                         </ul>
                 )
